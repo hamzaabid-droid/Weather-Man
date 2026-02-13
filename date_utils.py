0aa3e6c14a9bfa5d734646cdc3a294164ef33bf6
hamzaabid-droid <hamza.abid@arbisoft.com>
2026-02-13 11:28:04 +0500

    date utils

---
+++ date_utils.py
@@ -0,0 +1,18 @@
+
+
+from datetime import datetime
+
+
+def format_date(date_obj):
+    return date_obj
+
+
+
+def get_month_name(month_number):
+   
+    return month_number
+
+
+def parse_year_month(date_input):
+ 
+    return date_input
