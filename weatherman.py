66d09873a04dab7446eb2e1df9d343365a7d1064
hamzaabid-droid <hamza.abid@arbisoft.com>
2026-02-13 11:32:56 +0500

    Add files via upload

---
+++ weatherman.py
@@ -0,0 +1,38 @@
+
+
+import argparse
+from weather_parser import WeatherParser
+from printer import ReportPrinter
+from date_utils import parse_year_month
+class Weatherman:
+   
+
+    def __init__(self, data_dir):
+      
+        pass
+
+    def yearly_extremes(self, year):
+      
+        pass
+
+    def monthly_averages(self, year, month):
+    
+        pass
+
+    def daily_chart(self, year, month):
+       
+        pass
+
+
+def parse_args():
+
+    pass
+
+
+def main():
+   
+    pass
+
+
+if __name__ == "__main__":
+    main()
