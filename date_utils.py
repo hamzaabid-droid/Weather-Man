5908f0ad6f8c1a99db2d4e9467f3856c7b3cac3a
hamzaabid-droid <hamza.abid@arbisoft.com>
2026-02-13 16:42:08 +0500

    Enhance date utilities with formatting and month name
    Updated date formatting and added month name retrieval.

--- date_utils.py
+++ date_utils.py
@@ -1,18 +1,18 @@
-
-
 from datetime import datetime
-
+import calendar
 
 def format_date(date_obj):
-    return date_obj
+    return date_obj.strftime("%B %d")
 
+def get_month_name(month):
+    return calendar.month_name[month]
 
+def parse_year_month(value):
+    if "/" in value:
+        y, m = value.split("/")
+    elif "-" in value:
+        y, m = value.split("-")
+    else:
+        raise ValueError("Invalid date format")
 
-def get_month_name(month_number):
-   
-    return month_number
-
-
-def parse_year_month(date_input):
- 
-    return date_input
+    return int(y), int(m)
