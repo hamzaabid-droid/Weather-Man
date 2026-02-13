e87fb85862ac8a6164e1ab977362bbebf2d1fe4e
hamzaabid-droid <hamza.abid@arbisoft.com>
2026-02-13 16:37:27 +0500

    update file

--- weather_records.py
+++
@@ -1,7 +0,0 @@
-class WeatherRecord:
-  
-    def __init__(self, date, max_temp, min_temp, mean_humidity):
-        self.date = date                  
-        self.max_temp = max_temp          
-        self.min_temp = min_temp          
-        self.mean_humidity = mean_humidity  
